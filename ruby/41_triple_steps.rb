1af08a530039ba6e897361b5627e83f6e07e3b46
Joaquín Vicente <joakin@gmail.com>
2025-05-26 11:23:51 -0300

    ruby/41_triple_steps.rb

--- ruby/41_triple_steps.rb
+++ ruby/41_triple_steps.rb
@@ -6,7 +6,11 @@
 #
 
 def triple_step(n)
-  # TODO: implement function
+  return 0 if n <= 0
+  return 1 if n == 1
+  return 2 if n == 2
+  return 4 if n == 3
+  triple_step(n-1) + triple_step(n-2) + triple_step(n-3)
 end
 
 
