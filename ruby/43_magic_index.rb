391d9231b81bfd45270d8cb17aa4ad688a2a94a3
Joaquín Vicente <joakin@gmail.com>
2025-05-27 12:29:39 -0300

    ruby/43_magic_index.rb

--- ruby/43_magic_index.rb
+++ ruby/43_magic_index.rb
@@ -8,12 +8,34 @@
 
 # find_magic_index_distinct(arr: number[]): number | null {}
 def find_magic_index_distinct(arr)
-  # TODO: implement function
+  start = 0
+  last = arr.length - 1
+  pivot = arr.size / 2
+
+  while start <= last
+    return pivot if arr[pivot] == pivot
+    if arr[pivot] > pivot
+      last = pivot-1
+    else
+      start = pivot+1
+    end
+    pivot = (start + last)/2
+  end
+  return nil
 end
 
 # find_magic_index_non_distinct(arr: number[]): number | null {}
+def find_magic_index_non_distinct_rec(arr, start, last)
+  pivot = (start + last)/2
+  return pivot if arr[pivot] == pivot
+  return nil if start > last
+
+  find_magic_index_non_distinct_rec(arr, start, [pivot-1, [arr[pivot], 0].max].min) ||
+  find_magic_index_non_distinct_rec(arr, [pivot+1, arr[pivot]].max, last)
+end
+
 def find_magic_index_non_distinct(arr)
-  # TODO: implement function
+  find_magic_index_non_distinct_rec(arr, 0, arr.length - 1)
 end
 
 
@@ -21,11 +43,9 @@ RSpec.describe "magic index" do
   it "returns correct magic index for distinct input" do
     expect(find_magic_index_distinct([-2, -1, 0, 2, 4, 6, 8])).to eq(4) # Magic index: 4
 
-    expect(find_magic_index_distinct([-3, -2, -1, 4, 5, 7, 9])).to be false # No magic index
+    expect(find_magic_index_distinct([-3, -2, -1, 4, 5, 7, 9])).to be nil # No magic index
   end
-end
 
-RSpec.describe "not distinct" do
   it "returns correct magic index for non-distinct input" do
     # Test case where magic index exists
     expect(
@@ -35,6 +55,6 @@ RSpec.describe "not distinct" do
     # Test case where no magic index exists
     expect(
       find_magic_index_non_distinct([-10, -5, 0, 2, 5, 7, 9, 12, 13]),
-    ).to be false # No magic index
+    ).to be nil # No magic index
   end
 end
