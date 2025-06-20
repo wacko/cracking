c1de6587d7eb88ff90e56313103d067768581bf0
Joaquín Vicente <joakin@gmail.com>
2025-06-19 21:44:19 -0300

    javascript/03_urlify.test.ts

--- javascript/03_urlify.test.ts
+++ javascript/03_urlify.test.ts
@@ -5,37 +5,46 @@
 // and that you are given the "true" length of the string.
 
 export default function URLify (s1 : string): string {
-  // TODO: implement function
-  return "";
+  // This does not replace in-place
+  let url = "";
+  for (let i = 0; i < s1.length; i++) {
+    if (s1[i] == " ") {
+      url += "%20"
+    } else {
+      url += s1[i]
+    }
+  }
+
+  return url;
 }
 
 
 describe('03 - URLify', () =>{
-    test("Replaces spaces in a string with '%20'", () =>{
-        expect(URLify('ab c')).toEqual('ab%20c');
-    });
+  test("Replaces spaces in a string with '%20'", () =>{
+    expect(URLify('ab c')).toEqual('ab%20c');
+  });
 
-    test("Handles leading and trailing spaces", () =>{
-        expect(URLify('  ab c  ')).toEqual('%20%20ab%20c%20%20');
-    });
+  test("Handles leading and trailing spaces", () =>{
+    expect(URLify('  ab c  ')).toEqual('%20%20ab%20c%20%20');
+  });
 
-    test("Returns empty string when input is empty", () =>{
-        expect(URLify('')).toEqual('');
-    });
+  test("Returns empty string when input is empty", () =>{
+    expect(URLify('')).toEqual('');
+  });
 
-    test("Doesn't modify string without spaces", () =>{
-        expect(URLify('abc')).toEqual('abc');
-    });
+  test("Doesn't modify string without spaces", () =>{
+    expect(URLify('abc')).toEqual('abc');
+  });
 
-    test("Handles multiple consecutive spaces", () =>{
-        expect(URLify('a  b   c')).toEqual('a%20%20b%20%20%20c');
-    });
+  test("Handles multiple consecutive spaces", () =>{
+    expect(URLify('a  b   c')).toEqual('a%20%20b%20%20%20c');
+  });
 
-    test("Handles special characters", () =>{
-        expect(URLify('a b!c')).toEqual('a%20b!c');
-    });
+  test("Handles special characters", () =>{
+    expect(URLify('a b!c')).toEqual('a%20b!c');
+  });
 
-    test("Mr 3ohn Smith", () =>{
-        expect(URLify('Mr 3ohn Smith')).toEqual('Mr%203ohn%20Smith');
-    });
+  test("Mr 3ohn Smith", () =>{
+    expect(URLify('Mr 3ohn Smith')).toEqual('Mr%203ohn%20Smith');
+  });
 });
