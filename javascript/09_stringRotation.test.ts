c57f7cea8b16449fc14d408df4f71af3b141fc9b
Joaquín Vicente <joakin@gmail.com>
2025-06-20 20:53:48 -0300

    javascript/09_stringRotation.test.ts

--- javascript/09_stringRotation.test.ts
+++ javascript/09_stringRotation.test.ts
@@ -4,7 +4,15 @@
 // Given two strings, s1 and s2, write code to check if s2 is a rotation of s1 using only one call to isSubstring.
 // [e.g., "waterbottle" is a rotation of 'erbottlewat")
 
-
+/**
+ * Checks if a string is included in another string.
+ * It can only be used once.
+ * (used in problem 09: String rotation.)
+ *
+ * @param {string} s1 - String to compare
+ * @param {string} s2 - Substring.
+ * @return {boolean} If s1 contains s2.
+ */
 const createIsSubstring = () => {
   let called = false
 
@@ -15,21 +23,9 @@ const createIsSubstring = () => {
   }
 }
 
-/**
- * Checks if a string is included in another string.
- * It can only be used once.
- * (used in problem 09: String rotation.)
- *
- * @param {string} s1 - String to compare
- * @param {string} s2 - Substring.
- * @return {boolean} If s1 contains s2.
- */
-const isSubstring = createIsSubstring()
-
-
 export default function stringRotation(s1: string, s2: string): boolean {
-  // TODO: implement function
-  return true;
+  const isSubstring = createIsSubstring()
+  return isSubstring(s1+s1, s2)
 }
 
 
